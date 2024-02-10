1c2109dbccd17586d8ea4755649474379048c354
Joshua <joshua@Joshua-MBP2023.local>
2024-02-10 15:54:43 -0800

    added some comments

--- Btree.go
+++ Btree.go
@@ -12,9 +12,9 @@ type Key struct {
 
 // BTreeNode represents a node in the B-tree
 type BTreeNode struct {
-	leaf  bool
-	keys  []Key
-	child []*BTreeNode
+	leaf     bool
+	keys     []Key
+	children []*BTreeNode
 }
 
 // BTree represents the B-tree
@@ -36,13 +36,18 @@ func NewBTree(t int) *BTree {
 	}
 }
 
+//t = 3
+//insert 8
+//      5
+//  2 3    7 8, 9
+
 // Insert a key into the B-tree
 func (bt *BTree) Insert(key, val int) {
 	root := bt.root
 	if len(root.keys) == (2*bt.t)-1 {
 		newRoot := &BTreeNode{}
 		bt.root = newRoot
-		newRoot.child = append(newRoot.child, root)
+		newRoot.children = append(newRoot.children, root)
 		bt.splitChild(newRoot, 0)
 		bt.insertNonFull(newRoot, key, val)
 	} else {
@@ -65,26 +70,26 @@ func (bt *BTree) insertNonFull(x *BTreeNode, key, val int) {
 			i--
 		}
 		i++
-		if len(x.child[i].keys) == (2*bt.t)-1 {
-			bt.splitChild(x, i) // After splitting, the median key from the full child x.child[i] is moved up to the parent x at index i.
+		if len(x.children[i].keys) == (2*bt.t)-1 {
+			bt.splitChild(x, i) // After splitting, the median key from the full children x.children[i] is moved up to the parent x at index i.
 			if key > x.keys[i].key {
 				i++
 			}
 		}
-		bt.insertNonFull(x.child[i], key, val)
+		bt.insertNonFull(x.children[i], key, val)
 	}
 }
 
-// Split a child node of the B-tree
-// x: parent node, i: index of the child to be split.
+// Split a children node of the B-tree
+// x: parent node, i: index of the children to be split.
 func (bt *BTree) splitChild(x *BTreeNode, i int) {
 	t := bt.t                     // minimum degree
-	y := x.child[i]               // y is the node to be split
+	y := x.children[i]            // y is the node to be split
 	z := &BTreeNode{leaf: y.leaf} // a new node (right sibling of y, to put values split from y)
 
-	x.child = append(x.child[:i+1], nil)
-	copy(x.child[i+2:], x.child[i+1:])
-	x.child[i+1] = z // line79-81 inserts z(the new node) to the appropriate position in x.child(parent of y and z)
+	x.children = append(x.children[:i+1], nil)
+	copy(x.children[i+2:], x.children[i+1:])
+	x.children[i+1] = z // line79-81 inserts z(the new node) to the appropriate position in x.children(parent of y and z)
 
 	x.keys = append(x.keys[:i], Key{})
 	copy(x.keys[i+1:], x.keys[i:])
@@ -93,9 +98,9 @@ func (bt *BTree) splitChild(x *BTreeNode, i int) {
 	z.keys = append(z.keys, y.keys[t:]...) // halve y.keys, move the second part to z.keys
 	y.keys = y.keys[:t-1]                  // halve y.keys, and kept the first part
 
-	if !y.leaf { // halve y.child, more the second half to z.child
-		z.child = append(z.child, y.child[t:]...)
-		y.child = y.child[:t]
+	if !y.leaf { // halve y.children, move the second half to z.children
+		z.children = append(z.children, y.children[t:]...)
+		y.children = y.children[:t]
 	}
 }
 
@@ -118,11 +123,25 @@ func (bt *BTree) lookupKey(node *BTreeNode, key int) (bool, int) {
 		// Key not found in a leaf node
 		return false, 0
 	} else {
-		// Recursively search in the appropriate child
-		return bt.lookupKey(node.child[i], key)
+		// Recursively search in the appropriate children
+		return bt.lookupKey(node.children[i], key)
 	}
 }
 
+// nodes = [0x121212,0x121216,0x12124232,0x12134342,0x12134342]
+//
+// 0x121212
+// 0x121216
+// 0x12124232
+// 0x12134342
+// 0x12134342
+// [1, 2, 3, 4]
+// [x, x, x, x, x, x, x]
+//
+//	x
+//
+// x    x
+// x x  x  x
 // Display prints out the structure of the B-tree under the specified node.
 // 需要加工一下, 没太明白要怎么display
 func (bt *BTree) Display(node *BTreeNode, level int) {
@@ -133,7 +152,7 @@ func (bt *BTree) Display(node *BTreeNode, level int) {
 		}
 		fmt.Println()
 
-		for _, child := range node.child {
+		for _, child := range node.children {
 			bt.Display(child, level+1)
 		}
 	}
@@ -144,29 +163,30 @@ func (bt *BTree) inOrderTraversal(node *BTreeNode) {
 	if node != nil {
 		for i, key := range node.keys {
 			if !node.leaf {
-				bt.inOrderTraversal(node.child[i])
+				bt.inOrderTraversal(node.children[i])
 			}
 			fmt.Print(key, " ")
 		}
 		if !node.leaf {
-			bt.inOrderTraversal(node.child[len(node.keys)])
+			bt.inOrderTraversal(node.children[len(node.keys)])
 		}
 	}
 }
 
 func main() {
-	btree := NewBTree(3) // Create a B-tree with a minimum degree of 2
+	btree := NewBTree(7) // Create a B-tree with a minimum degree of 2
 
-	keys := []int{50, 30, 70, 10, 40, 60, 80, 20, 45, 65, 35, 55, 75, 90}
+	keys := []int{29, 41, 44, 62, 46, 49, 27, 76, 91, 30, 100, 47, 34, 53, 9, 45}
 	for _, key := range keys {
 		btree.Insert(key, key*2)
 	}
 
 	fmt.Println("In-order traversal of B-tree:")
 	btree.inOrderTraversal(btree.root)
+	fmt.Println()
 
-	fmt.Println("Find if key 80 exist")
-	fmt.Println(btree.Lookup(80))
+	fmt.Println("Find if key 100 exist")
+	fmt.Println(btree.Lookup(100))
 
 	fmt.Println("Find if key 99 exist")
 	fmt.Println(btree.Lookup(99))
